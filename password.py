72d3bb1f806b9d474df98cc44fcb25863f580705
mvorot <mikevorot.com>
2023-03-07 10:02:18 -0500

    Wrote decode function and updated the menu

--- password.py
+++ password.py
@@ -9,6 +9,11 @@ def encode(password):
         encoded += char
     return encoded
 
+def decode(encoded):
+    decoded_password = ''
+    for i in encoded:
+        decoded_password += str(int(i) - 3)
+    return decoded_password
 
 def main():
 
@@ -24,8 +29,10 @@ def main():
         if option == 1:
             password = input("Please enter the password to encode: ")
             encoded = encode(password)
-            print("Your password has been encoded and stored!")
-
+            print("\nYour password has been encoded and stored!\n")
+        elif option ==2:
+            decoded = decode(encoded)
+            print(f"\nThe encoded password is {encoded}, and the original password is {decoded}.")
         elif option == 3:
             exit()
 
